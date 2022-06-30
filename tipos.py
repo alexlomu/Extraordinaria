37cd61c0ab2def0cf62592991c97be76c2dcde4f
alexlomu <91721507+alexlomu@users.noreply.github.com>
2022-06-30 13:07:55 +0200

    funciones human y not human

--- tipos.py
+++ tipos.py
@@ -4,3 +4,23 @@ def types():
     types = ["human", "no human"]
 
     return types
+
+def human():
+    inteligencia = random.randint(3,7)
+    fuerza = random.randint(1,6)
+    velocidad = random.randint(2,5)
+    resistencia = random.randint(2,5)
+    energia = random.randint(1,6)
+    lucha =  random.randint(1,7)
+
+    return inteligencia, fuerza, velocidad, resistencia, energia, lucha
+
+def not_human():
+    inteligencia = random.randint(4,6)
+    fuerza = random.randint(1,7)
+    velocidad = random.randint(1,7)
+    resistencia = random.randint(3,7)
+    energia = random.randint(1,7)
+    lucha =  random.randint(3,6)
+
+    return inteligencia, fuerza, velocidad, resistencia, energia, lucha

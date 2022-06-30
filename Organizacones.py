f6f5fd91a640bca140d65fcb0a3afa0253dabde7
alexlomu <91721507+alexlomu@users.noreply.github.com>
2022-06-30 13:04:45 +0200

    prueba

--- Organizacones.py
+++ Organizacones.py
@@ -128,3 +128,38 @@ class Organizacion():
     def surrender(self):
         for i in self.salud:
             self.salud[i] == 0
+
+def prueba():
+    
+    print("=================================================================.")
+    print("Test Case 1: Create an Oganization.")
+    print("=================================================================.")
+    
+    organizacion_1 = Organizacion("Avengers", ["IronMan", "Capitán América", "Thor"], [100, 200, 500], [8, 7, 9], [7, 7, 10])
+
+    if organizacion_1.get_nombre() == "Avengers":
+        print("Test PASS. The parameter organization_name has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if organizacion_1.get_superheroes() == ["IronMan", "Capitán América", "Thor"]:
+        print("Test PASS. The parameter superheroes has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if organizacion_1.get_salud() == [100, 200, 500]:
+        print("Test PASS. The parameter health_points has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if organizacion_1.get_ataque() == [8, 7, 9]:
+        print("Test PASS. The parameter attack_rating has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if organizacion_1.get_defensa() == [7, 7, 10]:
+        print("Test PASS. The parameter defense_rating has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+prueba()

1c637fbadb1af2c8a440f81b402c8ee953e15b43
alexlomu <91721507+alexlomu@users.noreply.github.com>
2022-06-30 13:10:39 +0200

    gets y sets

--- Superheroes.py
+++ Superheroes.py
@@ -52,3 +52,95 @@ class Superheroes():
 
     def __del__(self):
          Superheroes.lista_ids.remove(self.id)
+
+    def get_id(self):
+        
+        return self.id
+
+
+    def get_alias(self):
+        
+        return self.get_alias
+
+
+    def get_secret_identity(self):
+        
+        return self.secret_identity
+
+
+    def get_movements(self):
+        
+        return self.movements
+
+
+    def get_type(self):
+        
+        return self.type
+
+
+    def get_cost(self):
+    
+        return self.cost
+
+    def get_energy(self):
+
+        return self.energy
+
+    def get_health(self):
+
+        return self.health
+
+
+    def set_alias(self, superhero_alias_to_be_set):
+        
+        if isinstance(superhero_alias_to_be_set, str):
+            self.alias = superhero_alias_to_be_set
+        else:
+            raise TypeError("The parameter superhero_alias_to_be_set should be a String.")
+
+    def set_secret_identity(self, superhero_secret_identity_to_be_set):
+        
+        if isinstance(superhero_secret_identity_to_be_set, str):
+            self.secret_identity = superhero_secret_identity_to_be_set
+        else:
+            raise TypeError("The parameter superhero_secret_identity_to_be_set should be a String.")
+    
+    def set_movements(self, superhero_movements_to_be_set):
+        
+        if isinstance(superhero_movements_to_be_set, list):
+            self.movements = superhero_movements_to_be_set
+        else:
+            raise TypeError("The parameter superhero_movements_to_be_set should be a String.")
+
+
+    def set_type(self, type_to_be_set):
+        
+        if isinstance(type_to_be_set, types):
+            self.type = type_to_be_set
+        else:
+            raise TypeError("The parameter type should be in types.")
+
+
+    def set_cost(self, cost_to_be_set):
+      
+        if isinstance(cost_to_be_set, int):
+           self.cost = cost_to_be_set
+        else:
+            raise TypeError("The parameter cost_to_be_set should be a int.")
+
+    def set_energy(self, energy_to_be_set):
+      
+        if isinstance(energy_to_be_set, int):
+           self.energy = energy_to_be_set
+        else:
+            raise TypeError("The parameter energy_to_be_set should be a int.")
+
+    def set_health(self, health_to_be_set):
+
+        if isinstance(health_to_be_set, int):
+            if 1 <= health_to_be_set <= 1000:
+                self.health = health_to_be_set
+            else:
+                raise ValueError("The parameter health_to_be_set should be > 0 and <= 1000.")
+        else:
+            raise TypeError("The parameter health_to_be_set should be a int.")
